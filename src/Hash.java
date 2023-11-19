f23688a19d4b865460a622c50db802da57aa4bd3
Suren_Hakobyan <surhak99@gmail.com>
2023-11-19 18:30:24 +0400

    Added hash function implementation.

--- src/Hash.java
+++ src/Hash.java
@@ -1,5 +1,8 @@
 public class Hash {
-    public static Long hash(String text){
-        return null;
+    public static Long hash(String text, int modulo){
+        long result = 0L;
+        for (int i = 0; i < text.length(); i++)
+            result += (int) text.charAt(i);
+        return result % modulo;
     }
 }
