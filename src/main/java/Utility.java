9dde7f6349e5307fd02af87fcbb90a198d0f5a41
Lusine Mkrtchyan <lusine_mkrtchyan3@edu.aua.am>
2023-11-21 14:31:32 +0400

    Added method for prime number generation

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -1,15 +1,50 @@
 package main.java;
+import java.util.ArrayList;
 
 public class Utility {
     public static Long generatePrime(){
-        return null;
+        int n = 168;
+        ArrayList<Long> list = firstNPrimes(n);
+        int index = (int)(Math.random() * n) + 1;;
+        return list.get(index);
     }
 
+    public static void main(String[] args) {
+        System.out.println(generatePrime());
+    }
     public static Long findPrimitiveRoot(Long prime){
         return null;
     }
 
     public static Long calculateInverse(Long n, Long modulo){
-        return null;
+//        do the prime factorization
+    return null;
+    }
+
+//    Utility to generate first n prime numbers
+    private static ArrayList<Long> firstNPrimes(int n){
+        ArrayList<Long> primes = new ArrayList<>();
+        for (long i = 1; i < 1000; i ++){
+//            if the number is 2 or it is not 1 and it isn't even
+            if(i == 2 || (i != 1 && i % 2 == 1)) {
+//                if the number is to then add to the list
+                if(i == 2) {
+                    primes.add(i);
+                }
+//                otherwise if the number is not 2
+                else {
+                    if (primes.size() == n) break;
+//                    loop over all numbers up to square root of i until finding a divisor
+//                    if reaches square root and doesn't find a divisor add the number to the list
+                    for(int j = 1; j <= (int) Math.sqrt(i); j ++){
+                        if(j > 1 && i % j == 0){
+                            break;
+                        }
+                        else if (j >= (int) Math.sqrt(i)) primes.add(i);
+                    }
+                }
+            }
+        }
+        return primes;
     }
 }
