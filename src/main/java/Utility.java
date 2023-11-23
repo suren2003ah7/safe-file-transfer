01878a7b6f6338cea65fbc9681233d35edb679ac
Lusine Mkrtchyan <lusine_mkrtchyan3@edu.aua.am>
2023-11-24 00:28:13 +0400

    Added methods to find primitive roots, modular inverse finder, and some other utilities

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -9,19 +9,59 @@ public class Utility {
         return list.get(index);
     }
 
-    public static Long findPrimitiveRoot(Long prime){
-        return null;
+    public static Long findPrimitiveRoot(long prime){
+        if (!isPrime(prime)) throw new IllegalArgumentException(prime + " is not a prime number");
+
+        int phi = (int)(prime - 1);
+        ArrayList<Long> primeFactors = primeFactors(phi);
+
+        for(int i = 2; i <= phi; i ++){
+            boolean fl = false;
+            for (Long num : primeFactors){
+                if(power(i, (int) (phi / num), (int)prime) == 1)
+                {
+                    fl = true;
+                    break;
+                }
+            }
+
+            if (!fl) return (long) i;
+
+        }
+
+        throw new IllegalArgumentException("Primitive root cannot be found");
     }
 
-    public static void main(String[] args) {
-        System.out.println(gcd(14, 24));
-    }
     public static Long calculateInverse(Long n, Long modulo){
-//        do the prime factorization
-    return null;
+
+//    using Extended Euclid's algorithm
+
+        Long m0 = modulo;
+        long y = 0, x = 1;
+
+        if (modulo == 1) return (long) 0;
+
+        while (n > 1) {
+            // q is quotient
+            long q = n / modulo, t = modulo;
+
+            // m is remainder now, continue using Euclid's algorithm
+            modulo = n % modulo;
+            n = t;
+            t = y;
+
+            // Update x and y
+            y = x - q * y;
+            x = t;
+        }
+
+        // Make x positive
+        if (x < 0) x += m0;
+
+        return x;
     }
 
-//    Method to compute GCD of two integers
+    //    Method to compute GCD of two integers
     public static long gcd(long a, long b){
         if (b == 0) return a;
         return gcd(b, a % b);
@@ -52,5 +92,50 @@ public class Utility {
             }
         }
         return primes;
+    }
+
+//    method to check if given number is prime
+    private static boolean isPrime(long n){
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+
+//        check for divisibility on 2 and 3 to skip some steps
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for(long i = 5; i  <= Math.sqrt(n); i += 5){
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+// Modular exponentiation
+    private static int power(int a, int pow, int mod) {
+
+        int res = 1; // Initialize result
+
+        //Update a if it is more than or
+        // equal to mod
+        a = a % mod;
+
+        while (pow > 0) {
+            // If pow is odd, multiply a with result
+            if ((pow & 1) == 1)
+                res = (res * a) % mod;
+
+            // pow should be even now
+            pow = pow >> 1; // pow = pow/2
+            a = (a * a) % mod;
+        }
+        return res;
+    }
+
+//    find prime factors of number n
+    private static ArrayList<Long> primeFactors(int n){
+        ArrayList<Long> list = firstNPrimes(n);
+        for(int i = 0; i < list.size(); i ++){
+            if((long)n % list.get(i) != 0) list.remove(i--);
+        }
+        return list;
     }
 }
